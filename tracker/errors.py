ac94e4a0107cde072f470857b6583cdd37fc7c23
Daniel Augusto <dev.daniel.augusto@gmail.com>
2023-01-13 20:49:03 -0300

    Add InvalidFlagError exception and fix naming

--- tracker/errors.py
+++ tracker/errors.py
@@ -2,5 +2,9 @@ class BaseTrackerException(Exception):
     """Core tackers exceptions."""
 
 
-class AppendOutOfScope(BaseTrackerException):
+class AppendOutOfScopeError(BaseTrackerException):
     """`append` must be called either by `success_dict` or `error_dict` methods."""
+
+
+class InvalidFlagError(BaseTrackerException):
+    """`create_csv` passed flag not recognized."""
