4c3124c9e7c565d2a2a92970a9a118f6e0109af6
Daniel Augusto <dev.daniel.augusto@gmail.com>
2023-01-13 20:42:43 -0300

    Fix exceptions docstrings and naming

--- tracker/errors.py
+++ tracker/errors.py
@@ -1,6 +1,6 @@
-class BaseTrackerError(Exception):
-    """Core Trackers exceptions."""
+class BaseTrackerException(Exception):
+    """Core tackers exceptions."""
 
 
-class AppendOutOfScope(BaseException):
+class AppendOutOfScope(BaseTrackerException):
     """`append` must be called either by `success_dict` or `error_dict` methods."""
