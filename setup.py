95317c4c872f87e9546fcda5ad80cf28adc2b659
Daniel Augusto <dev.daniel.augusto@gmail.com>
2023-01-14 18:29:52 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open('requirements/core.txt') as req:
-    req = req.read().splitlines()
 
-setup(
-    python_requires='>=3.8',
-    install_requires=(
-        *req,
-    ),
-)
+class ProjectSetup:
+    LICENSE = 'MIT'
+    REQUIRES_PYTHON = '>=3.8'
+
+    @classmethod
+    def init_setup(cls):
+        setup(
+            license=cls.LICENSE,
+            packages=find_packages(),
+            python_requires=cls.REQUIRES_PYTHON,
+            install_requires=(
+                *cls.get_requirements(),
+            ),
+        )
+
+    @staticmethod
+    def get_requirements() -> list:
+        with open('requirements/core.txt') as dependencies:
+            requirements = dependencies.read().splitlines()
+        return requirements[:-1]
+
+
+ProjectSetup.init_setup()
