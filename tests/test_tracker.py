d5dd6927d176b0ff8580dd3a196e41d2e7ce325b
Daniel Augusto <dev.daniel.augusto@gmail.com>
2023-01-14 15:04:26 -0300

    Fill missing tests

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,25 +1,27 @@
 import json
+import logging
 import pandas as pd
 import pytest
 import tracker as tk
 import typing as t
+from pytest_mock import MockerFixture
 from tests.utils import populate
 
 
 @pytest.fixture
 def tracker() -> tk.BaseTracker:
-    """Initialize a BaseTracker instance for testing purposes."""
+    """Initialize a specialized BaseTracker instance for testing purposes."""
     class Tracker(tk.BaseTracker):
 
         def __init__(self) -> None:
             super().__init__(('status', 'content'))
 
-        def error_dict(self, entry) -> None:
+        def error_dict(self, entry: pd.Series) -> None:
             """Overridden."""
             error_dict = {'status': entry[0], 'error': entry[1]}
             self.append_dict(error_dict)
 
-        def success_dict(self, entry) -> None:
+        def success_dict(self, entry: pd.Series) -> None:
             """Overridden."""
             success_dict = {'status': entry[0], 'message': 'Request filled with no errors.'}
             self.append_dict(success_dict)
@@ -30,9 +32,28 @@ def tracker() -> tk.BaseTracker:
                 kwargs['success'] = True if 199 < kwargs['status'] < 300 else False
             return kwargs
 
+        def _invalid_dict(self):
+            """Call append_dict to raise an exception."""
+            invalid_caller_dict = {'status': 0}
+            self.append_dict(invalid_caller_dict)
+
     _tracker = Tracker()
     populate(_tracker)
     return _tracker
+
+
+class TestBaseTracker:
+    """Carries BaseTracker class tests out without specialization."""
+
+    def test_transform(self, mocker: MockerFixture):
+        """Check if transform method does not change anything due to no specialization."""
+        _tracker = tk.BaseTracker(('status', 'content'))
+        transform_spy = mocker.spy(_tracker, 'transform')
+        add_snapshot_spy = mocker.spy(_tracker, 'add_snapshot')
+        kwargs = {'status': 200, 'content': json.dumps({'message': 'All fetched.'})}
+        _tracker.add_snapshot(**kwargs)
+        transform_spy.assert_called_with(**kwargs)
+        add_snapshot_spy.assert_called_with(**kwargs)
 
 
 class TestTracker:
@@ -87,3 +108,15 @@ class TestTracker:
         assert len(summary['failed']) == 3
         assert len(summary['succeded']) == 1
         assert all(isinstance(array, list) for array in arrays)
+
+    def test_log_summary(self, mocker: MockerFixture, tracker: tk.BaseTracker):
+        """Check if the loggger's info is called while invoking the tracker's summary."""
+        logger = logging.getLogger(__name__)
+        logger_spy = mocker.spy(logger, 'info')
+        tracker.log_summary(logger)
+        logger_spy.assert_called_once()
+
+    def test_append_dict_exception(self, tracker: tk.BaseTracker):
+        """Check if an exception is raised when append_dict is wrongly called."""
+        with pytest.raises(tk.errors.AppendOutOfScopeError):
+            tracker._invalid_dict()
