e2ca1c3c2e65291861a6769b79672b13a815c403
Daniel Augusto <dev.daniel.augusto@gmail.com>
2023-01-15 08:27:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 
 class ProjectSetup:
+    """Wraps project's settings as a package."""
 
+    NAME = 'tracker'
     LICENSE = 'MIT'
     REQUIRES_PYTHON = '>=3.8'
 
     @classmethod
-    def init_setup(cls):
+    def init_setup(cls) -> None:
+        """Run setup."""
         setup(
             license=cls.LICENSE,
             packages=find_packages(),
@@ -19,6 +22,7 @@ class ProjectSetup:
 
     @staticmethod
     def get_requirements() -> list:
+        """List only core libs to run the project."""
         with open('requirements/core.txt') as dependencies:
             requirements = dependencies.read().splitlines()
         return requirements[:-1]
